b8afcf21f7306574ef218a80516720ce67cc88b0
Vihan Jayaraman <vihanjay@gmail.com>
2022-12-06 16:20:45 -0800

    code

--- code.cpp
+++ code.cpp
@@ -1,5 +1,5 @@
-// Write your name
-// Write your perm number
+// Vihan Jayaraman
+// 7644545
 // Do not include a main function in this file
 // Only submit this file
 // See code.h for the descriptions of each problem
@@ -9,7 +9,23 @@
 /* Problem 1: 10 pts */
  
 TeaPacket* bestPacket(TeaPacket* head){
-   return 0;
+
+   if (head == NULL) {
+      return 0;
+   } else {
+      if (head->next!=NULL) {
+         TeaPacket* bestOfNextPackets = bestPacket(head->next);
+         int largestValue = bestOfNextPackets->rarity*bestOfNextPackets->rating;
+         if (largestValue > head->rarity*head->rating) {
+            return bestOfNextPackets;
+         } else {
+            return head; 
+         }
+      } else {
+         return head;
+      }
+   }
+
 }
 
 
@@ -17,13 +33,40 @@ TeaPacket* bestPacket(TeaPacket* head){
 /* Problem 2: 10 points*/
 
 Node* insert(Node* head, int value){
-   return 0;
+   Node* newNode = new Node();
+   newNode->data = value;
+
+   Node* currNode = head;
+
+   //gets node before newNode
+   while (currNode->data < newNode->data) {
+      currNode = currNode->next;
+   }
+   Node* nextNode = currNode->next;
+
+   currNode->next = newNode;
+   newNode->next = nextNode; 
+
+   return head;
+
 }
 
 /* Problem 3: 10 points*/
 
 bool isBalanced(std::string s){
    
+   int counter = 0;
+   for (int i = 0; i < s.length; i++) {
+      if (s[0] == 'A') {
+         counter++;
+      } else if (s[0] == 'B') {
+         counter--;
+      }
+   }
+   return (counter==0);
+}
+
+
    return false;
 }
 
